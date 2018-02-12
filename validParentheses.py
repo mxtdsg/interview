6c43bfb83c0869eb7924465eec2aba252ce40234
Xintian Meng <mxtdsg@hotmail.com>
2018-02-11 17:03:55 -0800

    check valid parentheses

---
+++ validParentheses.py
@@ -0,0 +1,17 @@
+####
+#
+# check '()','[]','{}'
+#
+# leetcode 20
+#
+####
+
+def isValid(self, s):
+    while '()' in s or '[]' in s or '{}' in s:
+        if '()' in s:
+            s=s.replace('()', '')
+        if '[]' in s:
+            s=s.replace('[]', '')  
+        if '{}' in s:
+            s=s.replace('{}', '')
+    return s==''
